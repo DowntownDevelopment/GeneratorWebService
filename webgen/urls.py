7fce1c0d376204418e3f60d17c0406b97c03714e
Lewis Menelaws <lmenelaws112@gmail.com>
2016-09-11 19:26:36 -0400

    Added new homepage
    lit fam

--- webgen/urls.py
+++ webgen/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', views.html_list, name='html_list')
+    url(r'^$', TemplateView.as_view(template_name="webgen/index.html"), name='main_page'),
+    url(r'^list$', views.html_list, name='html_list'),
 ]
