e065a2b89076f628194b4f801624d80ab871d3bf
Jasmyn Newton <jasmyn.n.newton@gmail.com>
2016-09-12 00:42:58 -0400

    Began layout for dashboard view
    Used bootstrap to begin designing a user dashboard view

--- webgen/views.py
+++ webgen/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render, render_to_response
 from django.utils import timezone
 from .models import RandomPage
@@ -6,3 +7,8 @@ from .models import RandomPage
 def html_list(request):
     html = RandomPage.objects.filter(date_generated__lte=timezone.now()).order_by('date_generated')
     return render(request, 'webgen/html_list.html', {'html': html})
+
+
+def dashboard(request):
+    html = RandomPage.objects.filter(date_generated__lte=timezone.now()).order_by('date_generated')
+    return render(request, 'webgen/dashboard.html', {'html': html})
