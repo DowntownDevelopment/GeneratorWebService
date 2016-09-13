14ac7ea4a2b6119a07906f29cf6f33a598b0fb4d
Jasmyn Newton <jasmyn.n.newton@gmail.com>
2016-09-13 02:11:16 -0400

    Created Gener8.html Template
    Updated structure of displaying html content from main.py functions

--- webgen/views.py
+++ webgen/views.py
@@ -8,13 +8,27 @@ from .generate import main
 
 
 def html_list(request):
+    print('Building List...')
     html = RandomPage.objects.filter(date_generated__lte=timezone.now()).order_by('date_generated')
     return render(request, 'webgen/html_list.html', {'html': html})
 
+
 def generate(request):
-    print('generating page')
+    print('Generating Page...')
     html_page = main.build_web_page()
     return render(request, 'webgen/generated_site.html', {'html_page': html_page})
+
+
 def dashboard(request):
+    print('Setting up Dashboard...')
     html = RandomPage.objects.filter(date_generated__lte=timezone.now()).order_by('date_generated')
     return render(request, 'webgen/dashboard.html', {'html': html})
+
+
+def gener8(request):
+    print('Generating Page...')
+    title = main.username()
+    content = main.content()
+    # css = main.css()
+    # js = main.js()
+    return render(request, 'webgen/gener8.html', {'title': title, 'content': content})
